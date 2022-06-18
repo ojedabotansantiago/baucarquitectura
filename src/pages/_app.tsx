01d6109d91ba5c639e8f3480cef20ece2991313a
santiago ojeda <ojedabotansantiago@gmail.com>
2022-06-18 12:22:57 +0200

    build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 function MyApp({ Component, pageProps }: AppProps) {
   const app = initializeApp(firebaseConfig);
 
